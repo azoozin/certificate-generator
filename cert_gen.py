ddb52c2fb6683781ed20ecadd1b057084c49fbf2
azoozin <155941153+azoozin@users.noreply.github.com>
2024-11-07 10:03:56 -0300

    add: function parameters
    Added function parameters to be able to customize the text generation via user input.

--- cert_gen.py
+++ cert_gen.py
@@ -14,18 +14,18 @@ def data_cleanup():
     except FileNotFoundError:
         print('Error: File name-list.txt not found')
 
-def generate_certs():
+def generate_certs(font_scale, template_file):
     if not os.path.exists('generated-certs'):
         os.makedirs('generated-certs')
         print('Folder for generated certificates was not found.\n')
         print('Folder for generated certificates was created.\n')
 
-    template_path = "cert-templates/template-cert.png"
+    template_path = f"cert-templates/{template_file}"
     if not os.path.exists(template_path):
         print(f"Error: Template not found in {template_path}")
         return
 
-    template = cv2.imread("cert-templates/template-cert.png")
+    template = cv2.imread(f"cert-templates/{template_file}")
     if template is None:
         print(f"Error: Unable to read template file at {template_path}")
         return
@@ -38,11 +38,11 @@ def generate_certs():
             output_path = os.path.join('generated-certs', f'{name}.png')
             # putText parameters:
             # (img to be used, string to insert, coordinate tuple, font, font scale, BGR color tuple, thickness, line type)
-            cv2.putText(cert, name, (515, 570), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 3, (0, 0, 0), 5, cv2.LINE_AA)
+            cv2.putText(cert, name, (515, 570), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, font_scale, (0, 0, 0), 5, cv2.LINE_AA)
             cv2.imwrite(output_path, cert)
             print(f'Certificate for {name} generated.')
         except Exception as e:
             print(f'Error: Could not generate certificate for {name}: {str(e)}')
 
-data_cleanup()
-generate_certs()
+# data_cleanup()
+# generate_certs()
